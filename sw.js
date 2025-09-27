ec400f8023a1ef0688ec4ccb12bd0a1ecbf44dc1
mabroouk <69696523+mabroouk@users.noreply.github.com>
2025-09-27 18:46:39 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+
+const CACHE_NAME = 'tawla-v11a-cache-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./sw.js'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request).catch(()=>resp))
+  );
+});
